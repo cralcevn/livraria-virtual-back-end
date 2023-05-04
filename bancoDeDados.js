8ba34a5fd6898b83f7075b00ff60ad3e1d2a7922
Nahara Cralcev <naharacralcevntp@gmail.com>
2023-05-03 21:34:34 -0300

    adicionar atualizações do projeto

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -7,7 +7,7 @@ try {
 
     await mongoose.connect(process.env.MONGO_URL)
 
-    console.log ("Conexão com o banco de daddos feita com sucesso")
+    console.log ("Conexão com o banco de dados feita com sucesso")
  } catch (erro) {
     console.log(erro)
    }
